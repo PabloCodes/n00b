efb9be888bb72ae2e521b31a3ce841b4c68eb1b1
Pablo Cazzulino <Pablo@MacBook-Pro.local>
2016-07-26 15:58:11 +0200

    points logged but not added to users

--- script.js
+++ script.js
@@ -16,6 +16,15 @@ app.config(function($routeProvider) {
 		controller: "LogInCtrl",
 		templateUrl: "templates/login.html"
 	})
+	// $routeProvider.when('/user/:userId', {
+	// 	controller: "BoardCtrl",
+	// 	templateUrl: "templates/home.html",
+	// 	resolve: {
+	// 		"currentAuth": function($firebaseAuth) {
+	// 			return $firebaseAuth().$requireSignIn();
+	// 		}
+	// 	}
+	// })
 });
 
 app.run(["$rootScope", "$location", function($rootScope, $location) {
@@ -28,27 +37,56 @@ app.run(["$rootScope", "$location", function($rootScope, $location) {
  });
 }]);
 
-app.controller("MainCtrl", function($scope, $http, $firebaseArray, $firebaseObject, $firebaseAuth, $location, $routeParams, $route){
+app.controller("MainCtrl", function($scope, $http, $firebaseArray, $firebaseObject, $firebaseAuth, $location, $routeParams, $route, currentAuth){
 	$scope.authObj = $firebaseAuth();
 
+	$scope.usersId = currentAuth.uid;
+	console.log($scope.usersId);
+	
+	//store noob points in board
+	var noobRef = firebase.database().ref().child("board").child($scope.usersId).child("noobCount");
+	$scope.noobPt = $firebaseArray(noobRef);
+	console.log($scope.noobPt);
 
+	//store assassin points in board
+	var assnRef = firebase.database().ref().child("board").child($scope.usersId).child("assnCount");
+	$scope.assnPt = $firebaseArray(assnRef);
+	
+	console.log($scope.assnPt);
 	//get users info in MainCtrl
 	var userRef = firebase.database().ref().child("users");
 	$scope.users = $firebaseObject(userRef);
 
-	console.log($scope.users.name);
-
 	//add noob points
-	$scope.noobPt = 0;
 	$scope.addNoobPt = function() {
-		$scope.noobPt = $scope.noobPt + 1;
-		console.log($scope.noobPt);
+		// //loop through array, incrementing count by 1
+		// for(var i = 0; i < $scope.noobPt.length; i++) {
+		// 	console.log("in the for loop");
+		// 	if ($scope.noobPt[i].count === NaN) {
+		// 		$scope.noobPt[i].count = 0;
+		// 	}
+		// 	else {
+		// 		$scope.noobPt[i].count = $scope.noobPt[i - 1].count + 1;	
+		// 	}
+		// };
+		// console.log($scope.noobPt[i].count)
+
+		console.log("I'm running");
+		$scope.noobPt.$add ({
+			count: $scope.noobPt[0].count,
+			sender: $scope.usersId,
+			created_at: Date.now()
+		});
+		
+
+		//simple counter
+		//$scope.noobPt = $scope.noobPt + 1;
+		//console.log($scope.noobPt);
 	}
 	//add assassin points
 	$scope.assnPt = 0;
 	$scope.addAssnPt = function() {
 		$scope.assnPt = $scope.assnPt + 1;
-		console.log($scope.assnPt);
 	}
 
 
@@ -73,16 +111,29 @@ app.controller("SignUpCtrl", function($scope, $http, $route, $firebaseObject, $f
 	$scope.signUp = function() {
 		console.log($scope.name + " " + $scope.email + " " + $scope.password);
 
+
 		console.log($scope.authObj);
 		$scope.authObj.$createUserWithEmailAndPassword($scope.email, $scope.password)
 		.then(function(firebaseUser) {
+			// $scope.points = {
+			// 	'$scope.noobPt': 0,
+			// 	'$scope.assnPt': 0,
+			// };
+			// console.log($scope.points);
 			console.log(firebaseUser.uid + " created successfully!");
 			var userRef = firebase.database().ref().child("users").child(firebaseUser.uid);
+
+			$scope.points = {
+				'$scope.noobPt': 0,
+				'$scope.assnPt': 0,
+			};
+			console.log($scope.points);
 
 			$scope.users = $firebaseObject(userRef);
 			$scope.users.name = $scope.name;
 			$scope.users.email = $scope.email;
 			$scope.users.password = $scope.password;
+			$scope.users.points = $scope.points;
 			$scope.users.$save();
 
 			console.log($scope.users);
